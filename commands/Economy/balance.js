4e983bf7a230263872c5315c78a1f6a1fe3bec55
byDeath ᴰᵉᵛ <99565725+DeathMFX@users.noreply.github.com>
2022-06-08 14:15:55 +0000

    Created prefix commands

---
+++ commands/Economy/balance.js
@@ -0,0 +1,39 @@
+const model = require('../../models/economy.js');
+const config = require('../../config/config.json');
+
+module.exports = {
+        name: 'balance',
+        description: 'Displays the user\'s balance.',
+        run: async (client, message) => {
+                const lang = message.member.guild.lang;
+                const user = message.mentions.users.first() || message.author;
+
+                model.findOne({
+                        User: user.id
+                }, (err, data) => {
+                        if (err) throw err;
+
+                        const wallet = data ? data.Wallet : 0;
+                        const bank = data ? data.Bank : 0;
+
+                        const embed = {
+                                thumbnail: {
+                                        url: user.avatarURL({ dynamic: true })
+                                },
+                                description: client.lang.__mf({ phrase: 'balance.embed', locale: lang }, { user: user.id }),
+                                fields: [
+                                        {
+                                                name: client.lang.__({ phrase: 'balance.embedField', locale: lang }),
+                                                value: client.lang.__mf({ phrase: 'balance.embedFieldValue', locale: lang }, { wallet: wallet, bank: bank })
+                                        }
+                                ],
+                                color: config.embedColor,
+                                timestamp: new Date()
+                        }
+
+                        message.reply({
+                                embeds: [embed]
+                        });
+                });
+        }
+}
--- handler/index.js
+++ handler/index.js
@@ -4,6 +4,19 @@ const globPromise = promisify(glob);
 const mongo = require('mongoose');
 
 module.exports = async (client) => {
+        const commandFiles = await globPromise(`${process.cwd()}/commands/*/*.js`);
+
+        commandFiles.map((value) => {
+                const file = require(value);
+                const splitted = value.split('/');
+                const directory = splitted[splitted.length - 2];
+
+                if (file.name) {
+                        const properties = { directory, ...file };
+                        client.commands.set(file.name, properties);
+                }
+        });
+
         const eventFiles = await globPromise(`${process.cwd()}/events/*/*.js`);
         eventFiles.map((value) => require(value));
 
